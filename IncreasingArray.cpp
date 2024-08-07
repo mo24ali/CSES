5506d78e3ca142847f1eca802a3334e6b6c3178d
Mo24ali <alichere1234@gmail.com>
2024-08-07 21:35:27 +0100

    commit 3

--- IncreasingArray.cpp
+++ IncreasingArray.cpp
@@ -4,8 +4,6 @@
 using namespace std;
 int main(){
     cout << "hellos worlsds"; //comment test valid
-    cout << "ayoow";
-      cout << "ayoow";
-      cout << "ayoww wtf";
+   
     return 0;
 }
